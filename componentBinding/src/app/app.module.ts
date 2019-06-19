e42fd2d0ce4cd142b328a6ef5f246e7ecc6d47d5
Sub2n <95su1208@gmail.com>
2019-06-19 16:24:04 +0900

    uncomfortable

--- componentBinding/src/app/app.module.ts
+++ componentBinding/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { AComponent } from './a/a.component';
-import { BComponent } from './b/b.component';
-import { CComponent } from './b/c/c.component';
+import { AComponent } from './a.component';
+import { BComponent } from './b.component';
+import { CComponent } from './c.component';
 
 @NgModule({
   declarations: [AppComponent, AComponent, BComponent, CComponent],
